b84d7ad0ea4c6876700cb11fb4c3479094d31b7b
pyjioh <pyjioh@gmail.com>
2015-12-27 22:05:14 +0500

    Add painting ability

--- js/index.js
+++ js/index.js
@@ -1,8 +1,64 @@
 /**
  * Created by pyjioh on 27.12.2015.
  */
-$( document ).ready(function() {
+$(document).ready(function () {
 
+    canvasDrawing.width = $('#canvasDiv').width();
+    canvasDrawing.height = $('#canvasDiv').height();
+    var context = canvasDrawing.getContext("2d");
 
+    $('#canvasDrawing').mousedown(function (e) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+
+        paint = true;
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        redraw();
+    });
+
+    $('#canvasDrawing').mousemove(function (e) {
+        if (paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    });
+
+    $('#canvasDrawing').mouseup(function (e) {
+        paint = false;
+    });
+
+    $('#canvasDrawing').mouseleave(function (e) {
+        paint = false;
+    });
+
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    var paint;
+
+    function addClick(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    }
+
+    function redraw() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+        context.lineJoin = "round";
+        context.lineWidth = 6;
+
+        for (var i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if (clickDrag[i] && i) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
 
 });
